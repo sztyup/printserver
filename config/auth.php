26bb575a4aeb9ae523b9b011bfd76e4dc980f7b5
sztyup <sztyup@gmail.com>
2018-04-11 18:14:47 +0200

    Add config

--- config/auth.php
+++ config/auth.php
@@ -66,7 +66,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'doctrine',
             'model' => App\User::class,
         ],
 
