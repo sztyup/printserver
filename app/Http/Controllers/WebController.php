c5b08f5597f45af950134bb10685c08b55714317
sztyup <sztyup@gmail.com>
2018-04-19 15:28:43 +0200

    Datatables controller

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Datatables\Printers;
 use App\Printing\Manager;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Sztyup\Datatable\DatatableInterface;
+use Sztyup\Datatable\DatatableResponse;
 
 class WebController extends Controller
 {
@@ -21,14 +25,46 @@ class WebController extends Controller
     }
 
     /**
-     * @param Manager $manager
+     * @param Request $request
+     * @param Printers $printers
      * @return \Illuminate\Contracts\View\View
      * @throws \Exception
      */
-    public function index(Manager $manager)
+    public function index(Request $request, Printers $printers)
     {
-        return $this->view('index', [
-            'printers' => $manager->getPrinters()[0]->getSn()
-        ]);
+        return $this->renderDatatables($request, $printers, 'index');
+    }
+
+    /**
+     * @param Request $request
+     * @param DatatableInterface $datatable
+     * @param $view
+     * @param array $viewData
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    protected function renderDatatables(Request $request, DatatableInterface $datatable, $view, $viewData = [])
+    {
+        $datatable->buildDatatable();
+
+        if ($request->method() == 'POST' && $request->isXmlHttpRequest()) {
+            $response = new DatatableResponse($request, $datatable);
+
+            try {
+                return $response->getResponse();
+            } catch (\Exception $e) {
+                if (config('app.debug')) {
+                    throw $e;
+                }
+
+                return response()->json([
+                    'error' => "\nHiba történt a táblázat megjelenítése közben"
+                ]);
+            }
+        } else {
+            return $this->view($view, array_merge($viewData, [
+                'dataTable' => $datatable
+            ]));
+        }
     }
 }
