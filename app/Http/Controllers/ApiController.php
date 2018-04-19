d1b281297cfd174db4acfb8291524260e25bb082
sztyup <sztyup@gmail.com>
2018-04-19 15:28:29 +0200

    Basic api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Printer;
+use App\Printing\Manager;
 use Illuminate\Routing\Controller;
 
 class ApiController extends Controller
 {
-    public function getPrinter()
+    /**
+     * @param Printer $printer
+     * @param Manager $manager
+     * @return mixed
+     */
+    public function getPrinter(Printer $printer, Manager $manager)
     {
+        return $manager->getPrinterBySn($printer->getSn());
     }
 
-    public function listPrinters()
+    /**
+     * @param Manager $manager
+     * @return \App\Printing\Printer[]
+     * @throws \Exception
+     */
+    public function listPrinters(Manager $manager)
     {
-
+        return $manager->getPrinters();
     }
 }
